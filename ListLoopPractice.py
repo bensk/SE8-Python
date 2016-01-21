7c0672f359c6728627fa3b97fa7e3469e2c22c99
bsk <bensamuelskalow@gmail.com>
2016-01-21 14:43:03 -0500

    extension

---
+++ ListLoopPractice.py
@@ -0,0 +1,28 @@
+import random
+my_randoms = random.sample(range(100), 15)
+
+print my_randoms
+
+def find_odds(x):
+    for numbers in x:
+        if numbers % 2 == 1:
+            print numbers
+
+def odd_sum(x):
+    odd_list = []
+    for numbers in x:
+        if numbers % 2 == 1:
+            odd_list.append(numbers)
+    return sum(odd_list)
+
+def even_sum(x):
+    even_list = []
+    for numbers in x:
+        if numbers % 2 == 0:
+            even_list.append(numbers)
+    return sum(even_list)
+
+print "The odd numbers are:"
+find_odds(my_randoms)
+print "The sum of the odd numbers is " + str(odd_sum(my_randoms))
+print "The sum of the even numbers is " + str(even_sum(my_randoms))
