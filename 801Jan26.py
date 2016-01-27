81e6c55989d269adb19fa6c92a819960af92ca77
SK <bsk@ms223.org>
2016-01-27 07:47:21 -0500

    yup

---
+++ 801Jan26.py
@@ -0,0 +1,27 @@
+def fizzbuzz():
+    for x in range(101):
+        print x
+
+fizzbuzz()
+
+# print 1
+2
+
+# import random
+# my_randoms = random.sample(range(100),101)
+# print my_randoms
+
+
+
+# # Make a test list
+# test_list = [1,2,3,4]
+# def even_sum(input):
+#     even_list=[]
+#     for x in input:
+#         if x % 2 == 0:
+#             even_list.append(x)
+#     print even_list
+#     print sum(even_list)
+#
+#
+# even_sum(test_list)
