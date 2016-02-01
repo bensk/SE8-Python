135c5f82068c2480dff3cbf1e76f420a861314ec
Mr. S-K <bsk@ms223.org>
2016-01-31 21:15:18 -0500

    welp

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,5 @@
 def rectangle():
+<<<<<<< Updated upstream
     import time
     x = int(input("How wide?"))
     y = int(input("How tall?"))
@@ -6,4 +7,10 @@ def rectangle():
         print "$" * x
     time.sleep(.01)
 
+=======
+    x = input("How wide?")
+    y = input("How tall?")
+    for numbers in range(y):
+        print "*" * x
+>>>>>>> Stashed changes
 rectangle()
